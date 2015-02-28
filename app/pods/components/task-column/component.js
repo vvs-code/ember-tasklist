3f4515f7de9b42f5a8fc96ea00d513f2025c89ab
Vasil Vanchuk <vasil.vanchuk@strevus.com>
2015-03-01 00:17:04 +0300

    Add component classes

--- app/pods/components/task-column/component.js
+++ app/pods/components/task-column/component.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   list: null, // pass in
-  status: null // pass in
+  status: null, // pass in,
+  filteredList: function() {
+    var list = this.get('list');
+    var status = this.get('status');
+
+    return list.filterBy('status', status);
+  }.property('list'),
+  statusClassName: function() {
+    return this.get('status').replace(/ /g,'-');
+  }.property('status'),
+  classNames: ['task-column'],
+  classNameBindings: ['statusClassName']
 });
