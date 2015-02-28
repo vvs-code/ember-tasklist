1bbd9d2fd9abbcc166c2c771f06c22996bb6207a
Vasil Vanchuk <vasil.vanchuk@strevus.com>
2015-02-28 13:22:00 +0300

    Create index, new tasklist routes

--- app/router.js
+++ app/router.js
@@ -6,7 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('tasklist', {path: '/'});
+  this.route('tasklist', {path: '/'}, function() {
+    this.route('new');
+  });
 });
 
 export default Router;
