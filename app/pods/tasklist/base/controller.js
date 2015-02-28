fc45ecce4423d209da3af30dd5d92376c94f2046
Vasil Vanchuk <vasil.vanchuk@strevus.com>
2015-02-28 14:22:33 +0300

    Allow create tasks

--- app/pods/tasklist/base/controller.js
+++ app/pods/tasklist/base/controller.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  taskStatuses: ['new', 'in process', 'done']
+  taskStatuses: ['new', 'in process', 'done'],
+  isValid: Ember.computed.notEmpty('model.title'),
+  actions: {
+    save: function() {
+      if(this.get('isValid')) {
+        var _this = this;
+        this.get('model').save().then(function() {
+          _this.transitionToRoute('tasklist');
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
+      return false;
+    }
+  }
 });
--- app/pods/tasklist/new/route.js
+++ app/pods/tasklist/new/route.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return this.store.createRecord('task');
+  },
+  actions: {
+    cancel: function() {
+      this.transitionTo('tasklist');
+      return false;
+    }
+  },
+  deactivate: function() {
+    var model = this.modelFor('tasklist/new');
+
+    if(model.get('isNew')) {
+      model.destroy();
+    }
+  }
 });
