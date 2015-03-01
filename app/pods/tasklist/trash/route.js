a9b87aad0c032d5b3c5082c6a0a6bc2c9d21a3c4
Vasil Vanchuk <vasil.vanchuk@strevus.com>
2015-03-01 22:33:24 +0300

    Display and clear removed tasks

--- app/pods/tasklist/trash/route.js
+++ app/pods/tasklist/trash/route.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function() {
+    return this.store.find('task', {status: 'removed'});
+  },
+  actions: {
+    clearTrash: function() {
+      this.modelFor('tasklist.trash').forEach(function(task){
+        task.destroyRecord();
+      });
+    }
+  }
 });
